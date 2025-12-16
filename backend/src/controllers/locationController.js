111534d87cf0b3fdbfeec3b8cd610f3ea9030c08
advaithdp02 <advaithdp@gmail.com>
2025-12-16 18:15:14 +0400

    ec2 mail fixes

--- backend/src/controllers/locationController.js
+++ backend/src/controllers/locationController.js
@@ -478,12 +478,15 @@ export const sendOTP = async (req, res) => {
   };
 
   const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // TLS
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
 
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
